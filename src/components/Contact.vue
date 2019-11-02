880a780c4eed518f63d20bbaa33ca5e647cc1fc3
Harriet <harriethw@gmail.com>
2019-11-02 14:56:39 +0000

    Adds content to Contact page

--- src/components/Contact.vue
+++ src/components/Contact.vue
@@ -1,10 +1,24 @@
 <script>
 export default {
-    name: 'Contact'
-}
+  name: "Contact"
+};
 </script>
 
 <template>
-    <div><h1>Placeholder for Contact</h1></div>
+<div>
+  <div class="title">
+      <h1>Contact Me</h1>
+    </div>
+  <div class="contact-body">
+    
+    <div class="contact-photo-wrapper">
+      <img class="contact-photo" src="@/assets/portrait.png" alt="portrait" />
+    </div>
+    <div class="contact-info">
+      <a href="mailto:harriethw@gmail.com">HarrietHW@gmail.com</a>
+      <a href="https://twitter.com/HarrietHW">@HarrietHW</a>
+    </div>
+  </div>
+</div>
 </template>
 
