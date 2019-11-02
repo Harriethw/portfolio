dd66f0982c38c5740a2d1e92e1a1b986e8d7d768
Harriet <harriethw@gmail.com>
2019-11-02 17:07:48 +0000

    First pass at pulling in data for Art page

--- src/components/Art.vue
+++ src/components/Art.vue
@@ -1,10 +1,30 @@
 <script>
+import artData from "@/assets/artData.json";
+import artSchema from "@/assets/artData.schema.json";
 export default {
-    name: 'Art'
-}
+  name: "Art",
+  data: function() {
+    return {
+      artData: artData,
+      artSchema: artSchema
+    };
+  }
+};
 </script>
 
 <template>
-    <div><h1>Placeholder for Art</h1></div>
+  <div class="art-body">
+    <div class="title">
+      <h1>Art</h1>
+    </div>
+    <div v-for="(art, index) in artData" :key="index">
+      <div
+        v-for="(key, index) in artSchema"
+        :key="index"
+        v-html="art[key.class]"
+        v-bind:class="key.class"
+      ></div>
+    </div>
+  </div>
 </template>
 
