66816337260a504759fe8b2dce59ce38c39cfebb
Harriet <harriethw@gmail.com>
2019-11-15 12:00:27 +0000

    initial styling of CV

--- src/components/CV.vue
+++ src/components/CV.vue
@@ -1,205 +1,29 @@
-<script>
-export default {
-  name: "CV"
-};
-</script>
-
 <template>
   <div class="CV-body">
-    <div>
-      <h1>Curriculum Vitae</h1>
+    <div v-for="(cv, index) in cvData" :key="index">
+      <div class="job-text">
+        <div
+          v-for="(key, index) in cvSchema"
+          :key="index"
+          v-html="cv[key.class]"
+          v-bind:class="key.class"
+        ></div>
+      </div>
     </div>
-    <p>
-      <span
-        style="font-size: 10pt; color: #666666; background-color: transparent; font-weight: 400; font-style: normal; font-variant: normal; text-decoration: none; vertical-align: baseline; white-space: pre-wrap;"
-      >American Express</span>Junior Software Engineer
-      <br />Brighton, Sussex, UK. July 2018 - present
-    </p>
-    <p>I worked on an innovative new initiative to improve data configuration for a set of microservices. Working on both front and back-end development (Java and Javascript), my role involved liaising with product owners, senior engineers and external stakeholders.</p>
-    <hr />
-    <p></p>
-    <p>
-      <span
-        style="font-size: 10pt; color: #666666; background-color: transparent; font-weight: 400; font-style: normal; font-variant: normal; text-decoration: none; vertical-align: baseline; white-space: pre-wrap;"
-      >Uken Games</span>Community Representative
-      <br />Toronto, ON. April 2017 - present
-    </p>
-    <p>At this leading Canadian mobile games company I worked to provide the best possible experience for our users, and to connect the developers and designers with player feedback. Writing FAQs, looking at analytics and investigating bugs, I had a close understanding of our products and the opportunities and challenges of mobile development.</p>
-    <hr />
-    <p>
-      Penguin Random House Consumer Marketing Coordinator
-      <br />Toronto, ON. July 2016 - April 2017
-    </p>
-    <p
-      style="line-height: 1.2; margin-top: 10pt; margin-bottom: 0pt;"
-    >I worked on brand direction for this new kind of bookstore, produced marketing materials and helped to run the shop itself.</p>
-    <ul style="margin-top: 0pt; margin-bottom: 0pt;">
-      <li
-        style="list-style-type: disc; font-size: 10pt; color: #000000; background-color: transparent; font-weight: 400; font-style: normal; font-variant: normal; text-decoration: none; vertical-align: baseline;"
-      >
-        <p
-          style="line-height: 1.2; margin-top: 10pt; margin-bottom: 0pt;"
-        >Writing copy for blogs, newsletters and social campaigns.</p>
-      </li>
-      <li
-        style="list-style-type: disc; font-size: 10pt; color: #000000; background-color: transparent; font-weight: 400; font-style: normal; font-variant: normal; text-decoration: none; vertical-align: baseline;"
-      >
-        <p
-          style="line-height: 1.2; margin-top: 0pt; margin-bottom: 0pt;"
-        >Overseeing operations and ensuring they run smoothly.</p>
-      </li>
-    </ul>
-    <hr />Toronto International Film Festival Facilitator, POPxTIFF
-    <br />Toronto, ON. June 2016 - Aug 2016
-    <p
-      style="line-height: 1.2; margin-top: 10pt; margin-bottom: 0pt;"
-    >I worked with the Exhibitions to team to ensure the smooth running of this innovative and challenging project, an exhibition of the world&rsquo;s most exciting virtual reality experiences. I am trained in the use of all major VR headsets.</p>
-    <ul style="margin-top: 0pt; margin-bottom: 0pt;">
-      <li
-        style="list-style-type: disc; font-size: 10pt; color: #000000; background-color: transparent; font-weight: 400; font-style: normal; font-variant: normal; text-decoration: none; vertical-align: baseline;"
-      >
-        <p
-          style="line-height: 1.2; margin-top: 10pt; margin-bottom: 0pt;"
-        >Problem-solving and communicating instructions and narrative information with hundreds of visitors a day.</p>
-      </li>
-      <li
-        style="list-style-type: disc; font-size: 10pt; color: #000000; background-color: transparent; font-weight: 400; font-style: normal; font-variant: normal; text-decoration: none; vertical-align: baseline;"
-      >
-        <p
-          style="line-height: 1.2; margin-top: 0pt; margin-bottom: 0pt;"
-        >Liaising with artists to ensure installations ran correctly.</p>
-      </li>
-    </ul>
-    <hr />
-    <p
-      style="line-height: 1.2; margin-top: 0pt; margin-bottom: 0pt; text-align: center;"
-    >Moved to Canada on a working IEC visa May 2016</p>
-    <hr />
-    <p style="line-height: 1.2; margin-top: 0pt; margin-bottom: 0pt;">
-      Penguin Random House Assistant Editor, Digital
-      <br />London, UK. April 2012 - May 2016
-    </p>
-    <p style="line-height: 1.2; margin-top: 10pt; margin-bottom: 0pt;">
-      As part of the digital publishing team, I handled a diverse portfolio of projects across fiction and interactive narratives. I produced ebooks, apps and websites. Projects included the web-based book
-      <a href="http://strata.lexrecords.com/" style="text-decoration: none;">STRATA</a>, a global arts and tech campaign for Stephen Fry called
-      <a href="http://yourfry.com/" style="text-decoration: none;">YourFry</a> and the
-      <a href="http://www.summerofpenguin.com" style="text-decoration: none;">Summer of Penguin</a> advertising campaign.
-    </p>
-    <ul style="margin-top: 0pt; margin-bottom: 0pt;">
-      <li
-        style="list-style-type: disc; font-size: 10pt; color: #000000; background-color: transparent; font-weight: 400; font-style: normal; font-variant: normal; text-decoration: none; vertical-align: baseline;"
-      >
-        <p
-          style="line-height: 1.2; margin-top: 10pt; margin-bottom: 0pt;"
-        >Writing copy for blogs, newsletters and social campaigns.</p>
-      </li>
-      <li
-        style="list-style-type: disc; font-size: 10pt; color: #000000; background-color: transparent; font-weight: 400; font-style: normal; font-variant: normal; text-decoration: none; vertical-align: baseline;"
-      >
-        <p
-          style="line-height: 1.2; margin-top: 0pt; margin-bottom: 0pt;"
-        >Managing content creation between authors and suppliers.</p>
-      </li>
-      <li
-        style="list-style-type: disc; font-size: 10pt; color: #000000; background-color: transparent; font-weight: 400; font-style: normal; font-variant: normal; text-decoration: none; vertical-align: baseline;"
-      >
-        <p
-          style="line-height: 1.2; margin-top: 0pt; margin-bottom: 0pt;"
-        >Copyediting and proofreading manuscripts.</p>
-      </li>
-      <li
-        style="list-style-type: disc; font-size: 10pt; color: #000000; background-color: transparent; font-weight: 400; font-style: normal; font-variant: normal; text-decoration: none; vertical-align: baseline;"
-      >
-        <p
-          style="line-height: 1.2; margin-top: 0pt; margin-bottom: 0pt;"
-        >Commissioning authors and artists.</p>
-      </li>
-      <li
-        style="list-style-type: disc; font-size: 10pt; color: #000000; background-color: transparent; font-weight: 400; font-style: normal; font-variant: normal; text-decoration: none; vertical-align: baseline;"
-      >
-        <p
-          style="line-height: 1.2; margin-top: 0pt; margin-bottom: 0pt;"
-        >Producing and overseeing video and audio production.</p>
-      </li>
-    </ul>
-    <hr />Springer Editorial and Production Assistant
-    <br />London, UK. May 2011 - March 2012
-    <p
-      style="line-height: 1.2; margin-top: 5pt; margin-bottom: 0pt;"
-    >At this science and business publishers, I worked closely with the editorial team to commission, manage and publish books and journals.</p>
-    <ul style="margin-top: 0pt; margin-bottom: 0pt;">
-      <li
-        style="list-style-type: disc; font-size: 10pt; color: #000000; background-color: transparent; font-weight: 400; font-style: normal; font-variant: normal; text-decoration: none; vertical-align: baseline;"
-      >
-        <p
-          style="line-height: 1.2; margin-top: 5pt; margin-bottom: 0pt;"
-        >Liaised with production department and external suppliers.</p>
-      </li>
-      <li
-        style="list-style-type: disc; font-size: 10pt; color: #000000; background-color: transparent; font-weight: 400; font-style: normal; font-variant: normal; text-decoration: none; vertical-align: baseline;"
-      >
-        <p
-          style="line-height: 1.2; margin-top: 0pt; margin-bottom: 0pt;"
-        >Processed contracts and departmental budgets.</p>
-      </li>
-      <li
-        style="list-style-type: disc; font-size: 10pt; color: #000000; background-color: transparent; font-weight: 400; font-style: normal; font-variant: normal; text-decoration: none; vertical-align: baseline;"
-      >
-        <p
-          style="line-height: 1.2; margin-top: 0pt; margin-bottom: 0pt;"
-        >Worked with XML, HTML, InDesign and Adobe Acrobat.</p>
-      </li>
-    </ul>
-    <hr />StrategyEye Reporter
-    <br />London, UK. Oct 2010 - March 2011
-    <ul style="margin-top: 0pt; margin-bottom: 0pt;">
-      <li
-        style="list-style-type: disc; font-size: 10pt; color: #000000; background-color: transparent; font-weight: 400; font-style: normal; font-variant: normal; text-decoration: none; vertical-align: baseline;"
-      >
-        <p
-          style="line-height: 1.2; margin-top: 5pt; margin-bottom: 0pt;"
-        >Wrote 7-10 articles a day for B2B strategists.</p>
-      </li>
-      <li
-        style="list-style-type: disc; font-size: 10pt; color: #000000; background-color: transparent; font-weight: 400; font-style: normal; font-variant: normal; text-decoration: none; vertical-align: baseline;"
-      >
-        <p
-          style="line-height: 1.2; margin-top: 0pt; margin-bottom: 0pt;"
-        >Updated the website and maintained records.</p>
-      </li>
-      <li
-        style="list-style-type: disc; font-size: 10pt; color: #000000; background-color: transparent; font-weight: 400; font-style: normal; font-variant: normal; text-decoration: none; vertical-align: baseline;"
-      >
-        <p
-          style="line-height: 1.2; margin-top: 0pt; margin-bottom: 0pt;"
-        >Researched digital media companies and events.</p>
-      </li>
-    </ul>
-    <hr />The Guardian Editorial Assistant
-    <br />London, UK. Aug 2006 - Aug 2007
-    <ul style="margin-top: 0pt; margin-bottom: 0pt;">
-      <li
-        style="list-style-type: disc; font-size: 10pt; color: #000000; background-color: transparent; font-weight: 400; font-style: normal; font-variant: normal; text-decoration: none; vertical-align: baseline;"
-      >
-        <p
-          style="line-height: 1.2; margin-top: 5pt; margin-bottom: 0pt;"
-        >Supported the production and editorial team with office administration, greeting clients and managing switchboard.</p>
-      </li>
-      <li
-        style="list-style-type: disc; font-size: 10pt; color: #000000; background-color: transparent; font-weight: 400; font-style: normal; font-variant: normal; text-decoration: none; vertical-align: baseline;"
-      >
-        <p
-          style="line-height: 1.2; margin-top: 0pt; margin-bottom: 0pt;"
-        >Outputted pages, production work and picture research.</p>
-      </li>
-      <li
-        style="list-style-type: disc; font-size: 10pt; color: #000000; background-color: transparent; font-weight: 400; font-style: normal; font-variant: normal; text-decoration: none; vertical-align: baseline;"
-      >
-        <p
-          style="line-height: 1.2; margin-top: 0pt; margin-bottom: 0pt;"
-        >Updated the website and the Guardian Weekly podcast.</p>
-      </li>
-    </ul>
   </div>
 </template>
 
+<script>
+import cvData from "@/assets/cvData.json";
+import cvSchema from "@/assets/cvData.schema.json";
+
+export default {
+  name: "CV",
+  data: function() {
+    return {
+      cvData: cvData,
+      cvSchema: cvSchema
+    };
+  }
+};
+</script>
---
+++ src/components/CV_old.vue
@@ -0,0 +1,129 @@
+<script>
+export default {
+  name: "CV"
+};
+</script>
+
+<template>
+  <div class="CV-body">
+    <div>
+      <h1>Curriculum Vitae</h1>
+    </div>
+    <p>
+      <span>American Express</span> Junior Software Engineer
+      <br />Brighton, Sussex, UK. July 2018 - present
+    </p>
+    <p>I worked on an innovative new initiative to improve data configuration for a set of microservices. 
+      Working on both front and back-end development (Java and Javascript), my role involved liaising with product owners, 
+      senior engineers and external stakeholders.</p>
+    <hr />
+    <p></p>
+    <p>
+      <span>Uken Games</span>Community Representative
+      <br />Toronto, ON. April 2017 - present
+    </p>
+    <p>At this leading Canadian mobile games company I worked to provide the best possible experience for our users, 
+      and to connect the developers and designers with player feedback. Writing FAQs, looking at analytics and investigating bugs, 
+      I had a close understanding of our products and the opportunities and challenges of mobile development.</p>
+    <hr />
+    <p>
+      Penguin Random House Consumer Marketing Coordinator
+      <br />Toronto, ON. July 2016 - April 2017
+    </p>
+    <p>I worked on brand direction for this new kind of bookstore, produced marketing materials and helped to run the shop itself.</p>
+    <ul>
+      <li>
+        <p>Writing copy for blogs, newsletters and social campaigns.</p>
+      </li>
+      <li>
+        <p>Overseeing operations and ensuring they run smoothly.</p>
+      </li>
+    </ul>
+    <hr />Toronto International Film Festival Facilitator, POPxTIFF
+    <br />Toronto, ON. June 2016 - Aug 2016
+    <p>I worked with the Exhibitions to team to ensure the smooth running of this innovative and challenging project, 
+      an exhibition of the world&rsquo;s most exciting virtual reality experiences. I am trained in the use of all major VR headsets.</p>
+    <ul>
+      <li>
+        <p>Problem-solving and communicating instructions and narrative information with hundreds of visitors a day.</p>
+      </li>
+      <li>
+        <p>Liaising with artists to ensure installations ran correctly.</p>
+      </li>
+    </ul>
+    <hr />
+    <p>Moved to Canada on a working IEC visa May 2016</p>
+    <hr />
+    <p>
+      Penguin Random House Assistant Editor, Digital
+      <br />London, UK. April 2012 - May 2016
+    </p>
+    <p>
+      As part of the digital publishing team, I handled a diverse portfolio of projects across fiction and interactive narratives. 
+      I produced ebooks, apps and websites. Projects included the web-based book
+      <a
+        href="http://strata.lexrecords.com/"
+      >STRATA</a>, a global arts and tech campaign for Stephen Fry called
+      <a href="http://yourfry.com/">YourFry</a> and the
+      <a href="http://www.summerofpenguin.com">Summer of Penguin</a> advertising campaign.
+    </p>
+    <ul>
+      <li>
+        <p>Writing copy for blogs, newsletters and social campaigns.</p>
+      </li>
+      <li>
+        <p>Managing content creation between authors and suppliers.</p>
+      </li>
+      <li>
+        <p>Copyediting and proofreading manuscripts.</p>
+      </li>
+      <li>
+        <p>Commissioning authors and artists.</p>
+      </li>
+      <li>
+        <p>Producing and overseeing video and audio production.</p>
+      </li>
+    </ul>
+    <hr />Springer Editorial and Production Assistant
+    <br />London, UK. May 2011 - March 2012
+    <p>At this science and business publishers, I worked closely with the editorial team to commission, manage and publish books and journals.</p>
+    <ul>
+      <li>
+        <p>Liaised with production department and external suppliers.</p>
+      </li>
+      <li>
+        <p>Processed contracts and departmental budgets.</p>
+      </li>
+      <li>
+        <p>Worked with XML, HTML, InDesign and Adobe Acrobat.</p>
+      </li>
+    </ul>
+    <hr />StrategyEye Reporter
+    <br />London, UK. Oct 2010 - March 2011
+    <ul>
+      <li>
+        <p>Wrote 7-10 articles a day for B2B strategists.</p>
+      </li>
+      <li>
+        <p>Updated the website and maintained records.</p>
+      </li>
+      <li>
+        <p>Researched digital media companies and events.</p>
+      </li>
+    </ul>
+    <hr />The Guardian Editorial Assistant
+    <br />London, UK. Aug 2006 - Aug 2007
+    <ul>
+      <li>
+        <p>Supported the production and editorial team with office administration, greeting clients and managing switchboard.</p>
+      </li>
+      <li>
+        <p>Outputted pages, production work and picture research.</p>
+      </li>
+      <li>
+        <p>Updated the website and the Guardian Weekly podcast.</p>
+      </li>
+    </ul>
+  </div>
+</template>
+
