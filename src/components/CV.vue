92a0ba2227ef2f79244dfb8440552df5ba80ccd3
Harriet <harriethw@gmail.com>
2019-11-15 15:27:40 +0000

    adding a skills tab for CV

--- src/components/CV.vue
+++ src/components/CV.vue
@@ -1,13 +1,40 @@
 <template>
   <div class="CV-body">
-    <div v-for="(cv, index) in cvData" :key="index">
-      <div class="job-text">
-        <div
-          v-for="(key, index) in cvSchema"
-          :key="index"
-          v-html="cv[key.class]"
-          v-bind:class="key.class"
-        ></div>
+    <div class="cv-sidebar">
+        <div v-if="skillsShow" class="skills-list">
+          <div class="skills-title">Skills</div>
+          <ul>
+            <li>C#, Java, Javascript</li>
+            <li>Vue, React</li>
+            <li>Firebase</li>
+            <li>SQL</li>
+            <li>GoCD, CircleCI</li>
+            <li>Spring</li>
+            <li>Unity</li>
+            <li>Prototyping, testing and</li>
+            <li>user research</li>
+            <li>Copywriting, editing </li>
+            <li> proofreading</li>
+
+            <br />
+            <li>Maya</li>
+            <li>Photoshop</li>
+            <li>iMovie</li>
+            <li>Audacity</li>
+          </ul>
+        </div>
+      <span @click="skillsShow = !skillsShow" class="title">+</span>
+    </div>
+    <div class="cv-content">
+      <div v-for="(cv, index) in cvData" :key="index">
+        <div class="job-text">
+          <div
+            v-for="(key, index) in cvSchema"
+            :key="index"
+            v-html="cv[key.class]"
+            v-bind:class="key.class"
+          ></div>
+        </div>
       </div>
     </div>
   </div>
@@ -22,7 +49,8 @@ export default {
   data: function() {
     return {
       cvData: cvData,
-      cvSchema: cvSchema
+      cvSchema: cvSchema,
+      skillsShow: false
     };
   }
 };
