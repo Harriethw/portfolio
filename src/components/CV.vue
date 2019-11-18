a3152d14d888a687ee3a36cf84554f1074321d18
harriethorobinworley <harriet.h@thoughtworks.com>
2019-11-18 17:24:42 +0000

    refines skills pop out

--- src/components/CV.vue
+++ src/components/CV.vue
@@ -1,20 +1,30 @@
 <template>
   <div class="CV-body">
     <div class="cv-sidebar">
+      <transition
+    enter-active-class="skills-enter-active"
+    leave-active-class="skills-leave-active"
+    @before-enter="beforeEnter"
+    @enter="enter"
+    @after-enter="afterEnter"
+    @before-leave="beforeLeave"
+    @leave="leave"
+    @after-leave="afterLeave"
+    mode="out-in"
+  >
       <div v-if="skillsShow" class="skills-list">
           <div class="skills-title">Skills</div>
           <ul>
-            <li>C#, Java, Javascript</li>
+            <li>C#, Java</li>
             <li>Vue, React</li>
             <li>Firebase</li>
             <li>SQL</li>
             <li>GoCD, CircleCI</li>
             <li>Spring</li>
             <li>Unity</li>
-            <li>Prototyping, testing and</li>
-            <li>user research</li>
-            <li>Copywriting, editing</li>
-            <li>proofreading</li>
+            <li>Prototyping</li>
+            <li>User Research</li>
+            <li>Copywriting</li>
 
             <br />
             <li>Maya</li>
@@ -23,7 +33,8 @@
             <li>Audacity</li>
           </ul>
       </div>
-      <span @click="skillsShow = !skillsShow" class="title">+</span>
+      </transition>
+      <span @click="skillsShow = !skillsShow" :class="{ skillsShow: skillsShow }" class="title skills-button">></span>
     </div>
     <div class="cv-content">
       <div v-for="(cv, index) in cvData" :key="index">
@@ -52,6 +63,62 @@ export default {
       cvSchema: cvSchema,
       skillsShow: false
     };
+  },
+  methods: {
+    /**
+     * @param {HTMLElement} element
+     */
+    beforeEnter(element) {
+      requestAnimationFrame(() => {
+        if (!element.style.height) {
+          element.style.width = '0px';
+        }
+
+        element.style.display = null;
+      });
+    },
+    /**
+     * @param {HTMLElement} element
+     */
+    enter(element) {
+      requestAnimationFrame(() => {
+        requestAnimationFrame(() => {
+          element.style.width = `${element.scrollWidth}px`;
+        });
+      });
+    },
+    /**
+     * @param {HTMLElement} element
+     */
+    afterEnter(element) {
+      element.style.width = null;
+    },
+    /**
+     * @param {HTMLElement} element
+     */
+    beforeLeave(element) {
+      requestAnimationFrame(() => {
+        if (!element.style.width) {
+          element.style.width = `${element.offsetWidth}px`;
+        }
+      });
+    },
+    /**
+     * @param {HTMLElement} element
+     */
+    leave(element) {
+      requestAnimationFrame(() => {
+        requestAnimationFrame(() => {
+          element.style.width = '0px';
+        });
+      });
+    },
+    /**
+     * @param {HTMLElement} element
+     */
+    afterLeave(element) {
+      element.style.width = null;
+    },
   }
 };
 </script>
