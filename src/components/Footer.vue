b8c29001144ba811bc15d78008f55694fe9e2a8b
harriethorobinworley <harriet.h@thoughtworks.com>
2019-11-20 15:46:13 +0000

    adds info to footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -2,13 +2,13 @@
   <div class="footer">
     <div class="footer-text">
       <ul>
-        <li>Site built with Vue, hosted by Heroku</li>
-        <li>Github</li>
-        <li>Itch.io</li>
-        <li>LinkedIn</li>
-        <li>Twitter</li>
-        <li>Email</li>
+        <li>Site built with Vue, hosted on Heroku</li>
+        <li> Find it on <a href="https://github.com/Harriethw/portfolio">GitHub</a></li>
+        <li>View more interactives on <a href="https://harriethw.itch.io/">Itch.io</a></li>
+        <li>Get professional on <a href="https://uk.linkedin.com/in/harriethw">LinkedIn</a></li>
+        <li>Connect on <a href="https://twitter.com/harriethw?lang=en">Twitter</a></li>
+        <li>Write to me on <a href="mailto:harriethw@gmail.com">Email</a></li>
       </ul>
     </div>
   </div>
-</template>s
+</template>
