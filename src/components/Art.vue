a78f0f132db89d49557316b3e73361c940bc1538
harriethorobinworley <harriet.h@thoughtworks.com>
2020-03-29 13:11:11 +0100

    Styling widths and borders for more coherency

--- src/components/Art.vue
+++ src/components/Art.vue
@@ -23,16 +23,12 @@ export default {
     <div class="title">
       <h1>A selection of my work</h1>
     </div>
-    <div
-      class="art-intro"
-    >I make interactive art, tools and apps that aim to delight and inspire people. <br> <br>
-    If you'd like to collaborate on a project, please get in touch :)</div>
+    <div class="art-intro">
+      I make interactive art, tools and apps that aim to delight and inspire people.
+      <br />
+      <br />If you'd like to collaborate on a project, please get in touch :)
+    </div>
     <div v-for="(art, index) in artData" :key="index" class="work">
-      <div class="work-image">
-        <carousel :value=0 :perPage="1" :paginationSize=15 paginationActiveColor="#eb8dd6" paginationColor="#ffb6c1">
-          <slide v-for="fileName in art.img" :key="fileName"><img :src="require('@/assets/' + fileName)" class="art-image" /></slide>
-        </carousel>
-      </div>
       <div class="work-text">
         <div
           v-for="(key, index) in artSchema"
@@ -43,6 +39,18 @@ export default {
         <div v-if="art['link']">
           <a :href="art['link']">{{ art['link'] }}</a>
         </div>
+      </div>
+      <div class="work-image">
+        <carousel
+          :perPage="1"
+          :paginationSize="15"
+          paginationActiveColor="#eb8dd6"
+          paginationColor="#ffb6c1"
+        >
+          <slide v-for="fileName in art.img" :key="fileName">
+            <img :src="require('@/assets/' + fileName)" class="art-image" />
+          </slide>
+        </carousel>
       </div>
     </div>
   </div>
