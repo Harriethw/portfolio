531da862691570f0e9e397f00edcd345bb658f6c
harriethorobinworley <harriethw@gmail.com>
2020-06-28 14:09:40 +0100

    attempt to fix server for vue router

--- server.js
+++ server.js
@@ -1,11 +1,16 @@
 /* eslint-disable */
 var express = require('express');
-var path = require('path');
+// var path = require('path');
+var history = require('connect-history-api-fallback')
 var serveStatic = require('serve-static');
-var history = require('connect-history-api-fallback');
 app = express();
+// Use a fallback for non-root routes (required for Vue router)
+//   NOTE: History fallback must be "used" before the static serving middleware!
+app.use(history({
+    // OPTIONAL: Includes more verbose logging
+    verbose: true
+}))
 app.use(serveStatic(__dirname + "/dist"));
-app.use(history);
 var port = process.env.PORT || 5000;
 app.listen(port);
 console.log('server started '+ port);
