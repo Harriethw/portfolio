ab2a072b1ec225aea9f34cd1038c87b9513241d7
Harriet HW <harriethw@gmail.com>
2020-10-24 15:41:28 +0100

    add forceSsl function
    To ensure it serves on https only - this might work!

--- server.js
+++ server.js
@@ -1,16 +1,24 @@
-/* eslint-disable */
-var express = require('express');
-// var path = require('path');
-var history = require('connect-history-api-fallback')
-var serveStatic = require('serve-static');
-app = express();
+const express = require('express');
+const history = require('connect-history-api-fallback')
+const serveStatic = require('serve-static');
+const env = process.env.NODE_ENV || 'development';
+
+const app = express();
+
+if(env === 'production') {
+    app.use((req, res, next) => {
+      if (req.header('x-forwarded-proto') !== 'https')
+        res.redirect(`https://${req.header('host')}${req.url}`)
+      else
+        next()
+    })
+}
 // Use a fallback for non-root routes (required for Vue router)
-//   NOTE: History fallback must be "used" before the static serving middleware!
 app.use(history({
-    // OPTIONAL: Includes more verbose logging
     verbose: true
 }))
 app.use(serveStatic(__dirname + "/dist"));
+
 var port = process.env.PORT || 5000;
 app.listen(port);
 console.log('server started '+ port);
