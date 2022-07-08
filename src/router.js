b947f9e7b38f47edd781e68bac6c7bebc73bab87
Harriet H-W <harriet.horobinworley@digital.trade.gov.uk>
2022-07-08 09:43:12 +0100

    remove experimental zdog page

--- src/router.js
+++ src/router.js
@@ -1,40 +1,34 @@
-import Vue from 'vue';
-import Router from 'vue-router';
-import CV from '@/components/CV.vue';
-import Art from '@/components/Art.vue';
-import Contact from '@/components/Contact.vue';
-import Zdog from '@/experiment/zDog.vue'
+import Vue from "vue";
+import Router from "vue-router";
+import CV from "@/components/CV.vue";
+import Art from "@/components/Art.vue";
+import Contact from "@/components/Contact.vue";
 
 Vue.use(Router);
 
 const router = new Router({
-    mode: 'history',
-    routes: [
-        {
-            path: '/',
-            redirect: '/art',
-        },
-        {
-            path: '/cv',
-            name: 'CV',
-            component: CV,
-        },
-        {
-            path: '/art',
-            name: 'Art',
-            component: Art,
-        },
-        {
-            path: '/contact',
-            name: 'Contact',
-            component: Contact,
-        },
-        {
-            path: '/zdog',
-            name: 'Zdog',
-            component: Zdog,
-        }
-    ]
+  mode: "history",
+  routes: [
+    {
+      path: "/",
+      redirect: "/art",
+    },
+    {
+      path: "/cv",
+      name: "CV",
+      component: CV,
+    },
+    {
+      path: "/art",
+      name: "Art",
+      component: Art,
+    },
+    {
+      path: "/contact",
+      name: "Contact",
+      component: Contact,
+    },
+  ],
 });
 
 export default router;
